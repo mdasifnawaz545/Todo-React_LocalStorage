19a4cc5e27508eba21ca28ed382589da8d0878e9
Md Asif Nawaz <126075328+mdasifnawaz545@users.noreply.github.com>
2024-06-20 09:14:14 +0530

    Update InputBox.jsx

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -12,7 +12,7 @@ function InputBox() {
     setInputValue('')
   }
   return (
-    <div>
+    <div className='mt-4rem'>
       <form onSubmit={(e) => { inputValue?handleSubmit(e):null }}  className='flex gap-4'>
         <input type="text" value={inputValue} onChange={(e)=>{setInputValue(e.target.value)}} className=' rounded-2xl outline-none shadow-lg  max-w-96 h-10 p-4 hover:scale-105 duration-300 hover:shadow-lg hover:shadow-blue-400 bg-blue-50' placeholder='Add your task' />
         <button type='submit' className='bg-[#67abfc] hover:shadow-2xl shadow-lg  text-white px-6 rounded-2xl hover:scale-105 duration-150'>Add</button>
